0a6f1f712ef82b8408dbd89e43f1338e16a972e5
riyalagare8-rgb <riyalagare8@gmail.com>
2026-01-17 10:15:12 +0530

    save

---
+++ power.py
@@ -0,0 +1,24 @@
+import sys
+def calculate_power(a,b)
+    exponent = 2(2)
+    if_name_=="main_"==:
+    print("calculate the power value")
+    if len (sys.argv[i])!=3
+    print("erreo:calaculate power value")
+    print("usage:python power <a> <b>")
+    try:
+        a=float(sys.argv[1])
+        b=float(sys.argv[2])
+        calculate_power({base} {expopnent})
+        print(calculate_power{base}{expopnen})
+        sys.exit(1)
+        
+    except Value:
+     print("error:the value should be in numerical")
+     sys.exit(1)
+
+
+
+
+
+
